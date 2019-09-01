1ff51818728c28078eba3ed755b51404503b8a0b
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-01 12:16:42 -0600

    Add initial noteListMain.js to display all notes

---
+++ src/noteListMain/noteListMain.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./noteListMain.css";
+
+class NoteListMain extends Component {
+  render() {
+    return (
+      <div className="noteListMain">
+        <h3 className="noteListMainTitle">All Notes:</h3>
+      </div>
+    );
+  }
+}
+
+export default NoteListMain;
