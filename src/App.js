d12c65e9117b663931f7844358f10ea864b4845f
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-01 11:30:41 -0600

    Add dummy-store.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "./header/header";
+import FolderList from "./folderList/folderList";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <FolderList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/folderList/folderList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import Folder from "~/Folder/Folder";
+import "./folderList.css";
+
+class folderList extends Component {
+  render() {
+    return (
+      <div className="folderList">
+        <h2>Folders:</h2>
+      </div>
+    );
+  }
+}
+
+export default folderList;
