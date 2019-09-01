164a228cd35d1a116e088a680bf718ba216c1942
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-01 16:14:58 -0600

    Add goBack button

---
+++ src/back/back.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./back.css";
+
+class Back extends Component {
+  render() {
+    return (
+      <div className="back">
+        <h2>Now Viewing Note</h2>
+        <button onClick={() => this.props.history.goBack()}>
+          Back to List
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Back;
