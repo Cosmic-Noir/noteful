908f2ab6425e10d43bee35c6f357ea734e7fbea7
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-01 14:56:11 -0600

    Fix display matching notes only issue

--- src/noteListFiltered/noteListFiltered.js
+++ src/noteListFiltered/noteListFiltered.js
@@ -3,12 +3,26 @@ import Note from ".././note/note";
 
 class NoteListFiltered extends Component {
   render() {
-    const matchingNotes = this.props.notes.find();
+    const matchingNotes = this.props.notes.map(note => {
+      if (note.folderId === this.props.match.params.folderID) {
+        return (
+          <Note
+            key={note.id}
+            id={note.id}
+            name={note.name}
+            modified={note.modified}
+          />
+        );
+      }
+    });
+
+    console.log(matchingNotes);
+    console.log(this.props);
+
     return (
       <div className="noteListMain">
         <h3 className="noteListMainTitle">Matching Folder Notes:</h3>
         {matchingNotes}
-
         <button className="addNote">Add Note</button>
       </div>
     );
