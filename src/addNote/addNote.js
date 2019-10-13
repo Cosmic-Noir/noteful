2892714732c39f6780833731a7fe733001fced69
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-13 15:01:01 -0600

    Update addNote to succesfully POST again

--- src/addNote/addNote.js
+++ src/addNote/addNote.js
@@ -10,35 +10,35 @@ class AddNote extends Component {
     super(props);
 
     this.state = {
-      noteTitle: {
+      title: {
         value: "",
         touched: false
       },
-      noteContent: {
+      content: {
         value: "",
         touched: false
       },
-      noteFolder: {
+      folder_id: {
         value: "",
         touched: false
       }
     };
 
-    this.noteTitle = React.createRef();
-    this.noteContent = React.createRef();
-    this.noteFolder = React.createRef();
+    this.title = React.createRef();
+    this.content = React.createRef();
+    this.folder_id = React.createRef();
   }
 
-  updateTitle(noteTitle) {
-    this.setState({ noteTitle: { value: noteTitle, touched: true } });
+  updateTitle(title) {
+    this.setState({ title: { value: title, touched: true } });
   }
 
-  updateContent(noteContent) {
-    this.setState({ noteContent: { value: noteContent, touched: true } });
+  updateContent(content) {
+    this.setState({ content: { value: content, touched: true } });
   }
 
-  updateFolder(noteFolder) {
-    this.setState({ noteFolder: { value: noteFolder, touched: true } });
+  updateFolder(folder_id) {
+    this.setState({ folder_id: { value: folder_id, touched: true } });
   }
 
   static defaultProps = {
@@ -47,57 +47,96 @@ class AddNote extends Component {
 
   static contextType = noteContext;
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const noteTitle = this.state.noteTitle.value;
-    const noteFolder = this.state.noteFolder.value;
-    const noteContent = this.state.noteContent.value;
-    let time = new Date();
-    time = time.toGMTString();
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   const noteTitle = this.state.noteTitle.value;
+  //   const noteFolder = this.state.noteFolder.value;
+  //   const noteContent = this.state.noteContent.value;
+  //   let time = new Date();
+  //   time = time.toGMTString();
 
-    this.postNote(noteTitle, noteFolder, noteContent, time);
-    this.props.history.push("/");
-  }
+  //   this.postNote(noteTitle, noteFolder, noteContent, time);
+  //   this.props.history.push("/");
+  // }
 
-  postNote = (title, folder, content, time) => {
-    const url = config.API_ENDPOINT + "notes";
-    let note = {
-      title: title,
-      folder_id: folder,
-      content: content,
-      date_published: time
+  // postNote = (title, folder, content, time) => {
+  //   const url = config.API_ENDPOINT + "notes";
+  //   let note = {
+  //     title: title,
+  //     folder_id: folder,
+  //     content: content,
+  //     date_published: time
+  //   };
+  //   fetch(url, {
+  //     method: "post",
+  //     headers: {
+  //       "content-type": "application/json"
+  //     },
+  //     body: JSON.stringify(note)
+  //   })
+  //     .then(res => {
+  //       if (!res.ok) {
+  //         throw new Error(res.status);
+  //       }
+  //       return res.json();
+  //     })
+  //     .then(() => {
+  //       this.context.getNotes();
+  //     })
+  //     // .then(console.log("Request succesful!"))
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
+  // };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { title, folder_id, content } = e.target;
+    const note = {
+      title: title.value,
+      folder_id: folder_id.value,
+      content: content.value
     };
-    fetch(url, {
-      method: "post",
+    console.log(note);
+
+    this.setState({ error: null });
+    fetch(config.API_ENDPOINT + "notes", {
+      method: "POST",
+      body: JSON.stringify(note),
       headers: {
         "content-type": "application/json"
-      },
-      body: JSON.stringify(note)
+      }
     })
       .then(res => {
         if (!res.ok) {
-          throw new Error(res.status);
+          return res.json().then(error => {
+            throw error;
+          });
         }
         return res.json();
       })
-      .then(() => {
-        this.context.getNotes();
+      .then(data => {
+        title.value = "";
+        folder_id.value = "";
+        content.value = "";
+        this.context.getNotes(data);
+        this.props.history.push("/");
       })
-      // .then(console.log("Request succesful!"))
       .catch(error => {
-        console.log(error);
+        this.setState({ error });
       });
   };
 
   validateTitle = () => {
-    const title = this.state.noteTitle.value.trim();
+    const title = this.state.title.value.trim();
     if (title.length === 0) {
       return "Title is required.";
     }
   };
 
   validateContent = () => {
-    const content = this.state.noteContent.value.trim();
+    const content = this.state.content.value.trim();
     if (content.length === 0) {
       return "Content is required.";
     }
@@ -118,46 +157,46 @@ class AddNote extends Component {
         }}
       >
         <h2>Add Note:</h2>
-        <label htmlFor="noteTitle">Title:</label>
+        <label htmlFor="title">Title:</label>
         <input
           type="text"
           className="noteTitle__input"
-          name="noteTitle"
-          id="noteTitle"
-          ref={this.noteTitle}
+          name="title"
+          id="title"
+          ref={this.title}
           onChange={e => this.updateTitle(e.target.value)}
           area-required="true"
           aria-describedby="addNote"
           aria-label="Enter note title"
         ></input>
-        {this.state.noteTitle.touched && (
+        {this.state.title.touched && (
           <ValidationError message={this.validateTitle()} />
         )}
-        <label htmlFor="noteFolder">Folder:</label>
+        <label htmlFor="folder_id">Folder:</label>
         <select
           className="noteFolder__input"
-          name="noteFolder"
-          id="noteFolder"
-          //   ref={this.noteFolder}
+          name="folder_id"
+          id="folder_id"
+          ref={this.folder_id}
           onChange={e => this.updateFolder(e.target.value)}
         >
           {folderOptions}
         </select>
-        <label htmlFor="noteContent" id="addContent">
+        <label htmlFor="content" id="content">
           Add Content:
         </label>
         <input
           type="text"
           className="noteContent__input"
-          name="noteContent"
-          id="noteContent"
-          ref={this.noteContent}
+          name="content"
+          id="content"
+          ref={this.content}
           onChange={e => this.updateContent(e.target.value)}
           aria-required="true"
           aira-describedby="addContent"
           aria-label="Enter note title"
         ></input>
-        {this.state.noteContent.touched && (
+        {this.state.content.touched && (
           <ValidationError message={this.validateContent()} />
         )}
         <button
